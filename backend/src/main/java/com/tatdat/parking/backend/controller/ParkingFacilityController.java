cf13ffce0e614f192a7163ce36a654da402cc33b
phongthanhdev <tranthanhphong20055@gmail.com>
2026-05-26 19:01:25 +0700

    Complete Parking Facility CRUD

--- backend/src/main/java/com/tatdat/parking/backend/controller/ParkingFacilityController.java
+++ backend/src/main/java/com/tatdat/parking/backend/controller/ParkingFacilityController.java
@@ -2,6 +2,7 @@ package com.tatdat.parking.backend.controller;
 
 import com.tatdat.parking.backend.entity.ParkingFacility;
 import com.tatdat.parking.backend.repository.ParkingFacilityRepository;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,4 +21,42 @@ public class ParkingFacilityController {
     public List<ParkingFacility> getAllParkingFacilities() {
         return parkingFacilityRepository.findAll();
     }
+
+    @PostMapping
+    public ParkingFacility createParkingFacility(@RequestBody ParkingFacility parkingFacility) {
+        return parkingFacilityRepository.save(parkingFacility);
+    }
+
+    @GetMapping("/{id}")
+    public ParkingFacility getParkingFacilityById(@PathVariable Integer id) {
+        return parkingFacilityRepository.findById(id).orElse(null);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteParkingFacility(@PathVariable Integer id) {
+
+        parkingFacilityRepository.deleteById(id);
+
+        return "Deleted Successfully";
+    }
+
+    @PutMapping("/{id}")
+    public ParkingFacility updateParkingFacility(
+            @PathVariable Integer id,
+            @RequestBody ParkingFacility updatedFacility
+    ) {
+        ParkingFacility parkingFacility =
+                parkingFacilityRepository.findById(id).orElse(null);
+
+        if (parkingFacility != null) {
+            parkingFacility.setFacilityName(updatedFacility.getFacilityName());
+            parkingFacility.setAddress(updatedFacility.getAddress());
+            parkingFacility.setStatus(updatedFacility.getStatus());
+
+            return parkingFacilityRepository.save(parkingFacility);
+        }
+
+        return null;
+    }
+
+
 }
